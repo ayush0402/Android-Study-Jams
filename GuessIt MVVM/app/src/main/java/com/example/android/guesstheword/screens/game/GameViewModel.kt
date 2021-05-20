b5f8c56defa01183ac14e62bb25530bee63166dd
ayush0402 <lit2020024@iiitl.ac.in>
2021-05-21 04:06:26 +0530

    Added viewModel & LiveData Arch.

---
+++ GuessIt MVVM/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,93 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel(){
+
+    // The current word
+    private val _word = MutableLiveData<String>()  /* encapsulating
+                                                    MutableLiveDate*/
+    val word : LiveData<String>
+        get()= _word
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get()=_eventGameFinish
+
+    init{
+        Log.i("GameViewModel","GameViewModel created!")
+        resetList()
+        nextWord()
+        _score.value=0
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel","GameViewModel Destroyed")
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            _eventGameFinish.value=true
+        } else {
+            _word.value = wordList.removeAt(0)
+        }
+    }
+     fun onSkip() {
+        _score.value=(_score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (_score.value)?.plus(1)
+        nextWord()
+    }
+
+    fun onGameFinishComplete(){
+        _eventGameFinish.value=false
+    }
+}
